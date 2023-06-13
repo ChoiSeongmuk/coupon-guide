7e96635c457132856c56744caca409903a1a2e89
ChoiSeongmuk <sawdonnie@icloud.com>
2023-06-13 13:44:26 +0900

    Test: for deploy test

--- src/pages/MobileGuide.js
+++ src/pages/MobileGuide.js
@@ -1,6 +1,14 @@
 import React from "react";
-
+import img1 from "../assets/img.gif";
 const GuidePage = () => {
-  return <div></div>;
+  const images = [img1];
+  return (
+    <div className="flex overflow-x-auto">
+      {images.map((image) => (
+        /* eslint-disable-next-line */
+        <img src={image} className="flex-none w-full h-screen" />
+      ))}
+    </div>
+  );
 };
 export default GuidePage;
