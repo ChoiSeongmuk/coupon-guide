81dbfd93871d9880207976caf30579456394e68f
ChoiSeongmuk <sawdonnie@icloud.com>
2023-06-14 18:57:53 +0900

    Refactor without detail

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Guide from "./pages/Guide";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import DetailPage from "./pages/DetailPage";
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path={"/"} element={<Guide />} />
-          <Route exact path={"/detail"} element={<DetailPage />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/Guide.js
+++ src/pages/Guide.js
@@ -1,15 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
+import img from "../assets/guide.jpeg";
 const Guide = () => {
-  const images = [];
+  const images = [img];
   return (
     <div className="justify-center">
-      <Link to={"/detail"} className="flex justify-end">
-        <label className="bg-blue-500 hover:bg-white hover:text-baharRed text-white font-semibold rounded-lg px-4 py-2 cursor-pointer">
-          자세히
-        </label>
-      </Link>
       <div className="justify-center">
         {images.map((image, index) => (
           /* eslint-disable-next-line */
